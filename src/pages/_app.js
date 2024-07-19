90c77511ad88f3ae08f10b2c7c17c619071ebabd
shah 911 <mianshahzeb911@gmail.com>
2024-07-19 19:36:58 +0500

    created 3 pages /about /contact /work

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,15 @@
 import "@/styles/globals.css";
+import { AnimatePresence } from "framer-motion";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps, router }) {
+  return (
+    <div className={inter.className}>
+      <AnimatePresence mode="wait">
+        <Component key={router.route} {...pageProps} />
+      </AnimatePresence>
+    </div>
+  );
 }
